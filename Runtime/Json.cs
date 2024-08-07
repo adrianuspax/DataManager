9c7d2759eb0d3674f9bea6b8fb9b6fcb627a897c
Adriano Paz <contato@adrianopaz.com>
2024-08-07 19:55:23 +0900

    Add custom path to Load and Save

--- Runtime/Json.cs
+++ Runtime/Json.cs
@@ -8,7 +8,7 @@ namespace ASP.DataManager
     /// </summary>
     public static class Json
     {
-        private static string dir,  subdir, file, jsonData;
+        private static string dir, subdir, file, jsonData;
 
         /// <summary>
         /// Saves the data in a json file on the user's machine
@@ -18,6 +18,21 @@ namespace ASP.DataManager
         public static void Save<T>(T data) where T : IDatable
         {
             subdir = UnityEngine.SceneManagement.SceneManager.GetActiveScene().name;
+            dir = $"{Application.persistentDataPath}/Data/{subdir}";
+            file = $"{dir}/{typeof(T)}.json";
+            Directory.CreateDirectory(dir);
+            jsonData = JsonUtility.ToJson(data, true);
+            File.WriteAllText(file, jsonData);
+        }
+        /// <summary>
+        /// Saves the data in a json file on the user's machine
+        /// </summary>
+        /// <typeparam name="T">Class that conforms to JsonUtility manipulation and is inherited from <see cref="IDatable"/></typeparam>
+        /// <param name="data">Instance of class <typeparamref name="T"/></param>
+        /// <param name="subDirectory">Path subdrictory to save</param>
+        public static void Save<T>(T data, string subDirectory) where T : IDatable
+        {
+            subdir = subDirectory;
             dir = $"{Application.persistentDataPath}/Data/{subdir}";
             file = $"{dir}/{typeof(T)}.json";
             Directory.CreateDirectory(dir);
@@ -48,6 +63,30 @@ namespace ASP.DataManager
             }
         }
         /// <summary>
+        /// Loads the data saved in a json file onto the user's machine
+        /// </summary>
+        /// <typeparam name="T">Class that conforms to JsonUtility manipulation and is inherited from <see cref="IDatable"/></typeparam>
+        /// <returns>Returns an instance of class <typeparamref name="T"/> with the data embedded</returns>
+        /// <param name="subDirectory">Path subdrictory to save</param>
+        public static T Load<T>(string subDirectory) where T : IDatable
+        {
+            subdir = subDirectory;
+            dir = $"{Application.persistentDataPath}/Data/{subdir}";
+            file = $"{dir}/{typeof(T)}.json";
+
+            if (File.Exists(file))
+            {
+                jsonData = File.ReadAllText(file);
+                T data = JsonUtility.FromJson<T>(jsonData);
+                return data;
+            }
+            else
+            {
+                Debug.LogWarning($"Json file not found in \"{file}\"");
+                return default;
+            }
+        }
+        /// <summary>
         /// Try Loads the data saved in a json file onto the user's machine
         /// </summary>
         /// <typeparam name="T">Class that conforms to JsonUtility manipulation and is inherited from <see cref="IDatable"/></typeparam>
@@ -56,6 +95,31 @@ namespace ASP.DataManager
         public static bool TryLoad<T>(out T data) where T : IDatable
         {
             subdir = UnityEngine.SceneManagement.SceneManager.GetActiveScene().name;
+            dir = $"{Application.persistentDataPath}/Data/{subdir}";
+            file = $"{dir}/{typeof(T)}.json";
+
+            if (File.Exists(file))
+            {
+                jsonData = File.ReadAllText(file);
+                data = JsonUtility.FromJson<T>(jsonData);
+                return true;
+            }
+            else
+            {
+                data = default;
+                return false;
+            }
+        }
+        /// <summary>
+        /// Try Loads the data saved in a json file onto the user's machine
+        /// </summary>
+        /// <typeparam name="T">Class that conforms to JsonUtility manipulation and is inherited from <see cref="IDatable"/></typeparam>
+        /// <param name="data">Instance of class</param>
+        /// <returns>True if there is a json file within the parameters set by the class that inherits from <see cref="IDatable"/></returns>
+        /// <param name="subDirectory">Path subdrictory to save</param>
+        public static bool TryLoad<T>(out T data, string subDirectory) where T : IDatable
+        {
+            subdir = subDirectory;
             dir = $"{Application.persistentDataPath}/Data/{subdir}";
             file = $"{dir}/{typeof(T)}.json";
 
